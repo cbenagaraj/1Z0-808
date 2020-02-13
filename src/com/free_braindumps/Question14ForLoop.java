7d8d09a15b4f85bd372b57db1a8dc35ef5f048ae
Nagarajan Chandramohan <cbenagaraj86@gmail.com>
2020-02-13 10:37:28 +0100

    for loop must and optional expressions testing

---
+++ src/com/free_braindumps/Question14ForLoop.java
@@ -0,0 +1,14 @@
+package com.free_braindumps;
+
+public class Question14ForLoop {
+
+	public static void main(String[] args) {
+		int i =0;
+		for(;i<5;) {
+			System.out.println("i is" + i);
+			i = i+1;
+		}
+
+	}
+
+}
